e117bdd6cca8e09df7b00d0fa72aa22acb1a3c46
abhijeetjawale1 <97281215+abhijeetjawale1@users.noreply.github.com>
2022-02-07 10:10:31 +0530

    End of section 9

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
     canActivate:[AuthGuard],
     children:[
       {path:'members',component:MemberListComponent,canActivate:[AuthGuard]},
-      {path:'members/:id',component:MemberDatailComponent},
+      {path:'members/:username',component:MemberDatailComponent},
       {path:'lists',component:ListsComponent},
       {path:'messages',component:MessagesComponent},
     ]
