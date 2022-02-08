ddc4c035788880f7f473eb247f3207b5c409a697
abhijeetjawale1 <97281215+abhijeetjawale1@users.noreply.github.com>
2022-02-08 18:02:40 +0530

    End of section 12

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,9 +18,11 @@ namespace API.Controllers
     {
         public DataContext _context;
         private readonly ITokenService _tokenService;
+        private readonly IMapper _mapper;
 
-        public AccountController(DataContext context,ITokenService tokenService)
+        public AccountController(DataContext context,ITokenService tokenService,IMapper mapper)
         {
+            _mapper = mapper;
             _context = context;
             _tokenService = tokenService;
         }
@@ -29,19 +32,24 @@ namespace API.Controllers
         {
             if(await UserExists(registerDto.Username))return BadRequest("Username Is Taken ");
 
+            var user=_mapper.Map<AppUser>(registerDto);
+
             using var hmac=new HMACSHA512();
-            var user=new AppUser{
-                UserName=registerDto.Username.ToLower(),
-                PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-                PasswordSalt=hmac.Key
-            };
+        
+                user.UserName=registerDto.Username.ToLower();
+                user.PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+                user.PasswordSalt=hmac.Key;
+       
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
             return new UserDto
             {
                 Username=user.UserName,
-                Token=_tokenService.CreateTokan(user)
+                Token=_tokenService.CreateTokan(user),
+                KnownAs=user.KnownAs,
+                
+
             };
            
         }
@@ -49,7 +57,8 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>>Login(LoginDto loginDto)
         {
-            var user=await _context.Users.SingleOrDefaultAsync(x=>x.UserName==loginDto.Username);
+            var user=await _context.Users.Include(p=>p.Photos)
+            .SingleOrDefaultAsync(x=>x.UserName==loginDto.Username);
             if(user==null) return Unauthorized("Invalid Username");
         
         using var hmac=new HMACSHA512(user.PasswordSalt);
@@ -60,7 +69,9 @@ namespace API.Controllers
            return new UserDto
             {
                 Username=user.UserName,
-                Token=_tokenService.CreateTokan(user)
+                Token=_tokenService.CreateTokan(user),
+             PhotoUrl=user.Photos.FirstOrDefault(x=>x.IsMain)?.Url,
+             KnownAs=user.KnownAs
             };
            
         }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,15 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
-import{ BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDatailComponent } from './members/member-datail/member-datail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component'
-import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
@@ -24,6 +22,9 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { TextInputComponent } from './_forms/text-input/text-input.component';
+import { DateInputComponent } from './_forms/date-input/date-input.component';
 
 
 @NgModule({
@@ -40,7 +41,10 @@ import { LoadingInterceptor } from './_interceptors/loading.interceptor';
     NotFoundComponent,
     ServerErrorComponent,
     MemberCardComponent,
-    MemberEditComponent
+    MemberEditComponent,
+    PhotoEditorComponent,
+    TextInputComponent,
+    DateInputComponent
   ],
   imports: [
     BrowserModule,
@@ -48,6 +52,7 @@ import { LoadingInterceptor } from './_interceptors/loading.interceptor';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule,
     NgxSpinnerModule
   ],
