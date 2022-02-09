86cf4b53ffe9d555e5ba0a7c516e21b67329f3c8
abhijeetjawale1 <97281215+abhijeetjawale1@users.noreply.github.com>
2022-02-09 18:08:13 +0530

    half section 13

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 
 namespace API.Interfaces
 {
@@ -14,8 +15,9 @@ namespace API.Interfaces
         Task<IEnumerable<AppUser>> GetAppUsersAsync();
         Task<AppUser>GetUserByIdAsync(int id);
         Task<AppUser>GetUserByUsernameAsync(string username);
-        Task<IEnumerable<MemberDto>>GetMembersAsync();
+        Task<PagedList<MemberDto>>GetMembersAsync(UserParams userParams);
         Task<MemberDto>GetMemberAsync(string username);
+        // Task<string> GetUserGender(string username);
 
     }
 }
